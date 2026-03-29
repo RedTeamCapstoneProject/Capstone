ef8af624ec8288c32958a5df83798eb94fa6ae06
jdchunn <jdchunn9280@gmail.com>
2026-03-29 11:27:27 -0500

    summary table

--- database/migrations/003_summary.sql
+++ database/migrations/003_summary.sql
@@ -1,12 +1,13 @@
-CREATE TABLE summary (
+CREATE TABLE IF NOT EXISTS summary (
     id BIGSERIAL PRIMARY KEY,
-    source_names VARCHAR(255),
-    authors VARCHAR(255),
-    AI_title TEXT,
-    AI_description TEXT,
-    urls VARCHAR(255),
-    category TEXT,
-    topic TEXT,
-    summary TEXT,
+    source_names TEXT[] NOT NULL,
+    authors TEXT[] NOT NULL,
+    ai_title TEXT,
+    ai_description TEXT,
+    urls TEXT[] NOT NULL,
+    category TEXT NOT NULL,
+    topic TEXT NOT NULL,
+    summary TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
